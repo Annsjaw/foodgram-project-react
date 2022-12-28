4aa7893f24dfd91309136e6383e8b154fa4c8a63
Dmitry Rotanin <rotanin@gmail.com>
2022-12-29 04:02:05 +0600

    fix

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -33,7 +33,6 @@ AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
+    'users',
     'recipes',
     'api',
     'import_export'
