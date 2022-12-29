8f9db13bf86b5bb8b64b24f8eee8d330c4a2fd3b
Dmitry Rotanin <rotanin@gmail.com>
2022-12-29 18:26:29 +0600

    add user router

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from users.views import CustomUserViewSet
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 v1_router = DefaultRouter()
+v1_router.register('users', CustomUserViewSet, basename='users')
 v1_router.register('tags', TagViewSet, basename='tags')
 v1_router.register('ingredients', IngredientViewSet, basename='ingredients')
 v1_router.register('recipes', RecipeViewSet, basename='recipes')
